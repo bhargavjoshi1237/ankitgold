7a5ac4e8dbe77f953a207d378573afbeb6352280
Bhargav Joshi <68766403+bhargavjoshi1237@users.noreply.github.com>
2023-10-04 03:59:19 -0700

    Add files via upload

---
+++ this.js
@@ -0,0 +1,58 @@
+
+  let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 3500); // Change image every 2 seconds
+}
+const next = document.querySelector(".next");
+const prev = document.querySelector(".previous");
+
+const numImg = document.querySelectorAll("img").length;
+let currImg = 1;
+
+let timeoutID;
+
+next.addEventListener("click", () => {
+  currImg++;
+  clearTimeout(timeoutID);
+  updateImage();
+});
+
+prev.addEventListener("click", () => {
+  currImg--;
+  clearTimeout(timeoutID);
+
+  updateImage();
+});
+
+const imgcontainer = document.querySelector(".img-container");
+
+function updateImage() {
+  if (currImg > numImg) {
+    currImg = 1;
+  } else if (currImg < 1) {
+    currImg = numImg;
+  }
+  imgcontainer.style.transform = `translateX(-${(currImg - 1) * 800}px)`;
+
+  timeoutID = setTimeout(() => {
+    currImg++;
+    updateImage();
+  }, 2000);
+}
+
+updateImage();
